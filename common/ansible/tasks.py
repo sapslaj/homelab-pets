c37f5ab0694879c0e45531f14b4abef1e0d2eff2
Justin Roberson <saps.laj@gmail.com>
2022-01-25 22:12:01 -0500

    update shit

--- common/ansible/tasks.py
+++ common/ansible/tasks.py
@@ -1,5 +1,11 @@
 from invoke import task
 
+
 @task
 def apply(c):
-    c.run("ansible-playbook -i eris.homelab.sapslaj.com, main.yml")
+    c.run("ansible-playbook -i inventory main.yml")
+
+
+@task
+def update(c):
+    c.run("ansible-playbook -i inventory update.yml")
