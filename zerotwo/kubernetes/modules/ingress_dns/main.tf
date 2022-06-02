49d34115e5caa0a4299681d803fbafbf1efe06e5
sapslaj <saps.laj@gmail.com>
2022-06-02 02:21:08 -0400

    switch to cname

--- zerotwo/kubernetes/modules/ingress_dns/main.tf
+++ zerotwo/kubernetes/modules/ingress_dns/main.tf
@@ -2,28 +2,10 @@ data "aws_route53_zone" "sapslaj_xyz" {
   name = "sapslaj.xyz"
 }
 
-data "dns_a_record_set" "homelab" {
-  host = "homelab.sapslaj.com"
-}
-
-locals {
-  dns_a_records         = data.dns_a_record_set.homelab.addrs
-  dns_a_records_private = ["172.24.4.15"]
-  dns_aaaa_records      = ["2001:470:e022:4:5054:ff:feea:5416"]
-}
-
-resource "aws_route53_record" "aaaa" {
-  name    = var.name
-  ttl     = 300
-  type    = "AAAA"
+resource "aws_route53_record" "cname" {
+  name = var.name
+  ttl = 300
+  type = "CNAME"
   zone_id = data.aws_route53_zone.sapslaj_xyz.zone_id
-  records = local.dns_aaaa_records
-}
-
-resource "aws_route53_record" "a" {
-  name    = var.name
-  ttl     = 300
-  type    = "A"
-  zone_id = data.aws_route53_zone.sapslaj_xyz.zone_id
-  records = local.dns_a_records_private
+  records = ["zerotwo.sapslaj.xyz"]
 }
