7dc456e3c68c4949445ccf8bf56994854e9a751b
sapslaj <saps.laj@gmail.com>
2022-06-16 21:51:29 -0400

    lil tweak

--- common/static-dns/modules/record_set/ipv6_to_ptr.py
+++ common/static-dns/modules/record_set/ipv6_to_ptr.py
@@ -8,12 +8,11 @@ query = json.load(sys.stdin)
 ip = query["ipv6"]
 partitions = ip.split("::")
 expanded = partitions[0].split(":")
-host_groups = []
 if len(partitions) == 2:
     host_groups = partitions[1].split(":")
-for _ in range(8 - len(expanded) + len(host_groups)):
-    expanded.append("0000")
-expanded.extend(host_groups)
+    for _ in range(8 - len(expanded) + len(host_groups)):
+        expanded.append("0000")
+    expanded.extend(host_groups)
 expanded = [group.rjust(4, "0") for group in expanded]
 ptr = ".".join(reversed(list("".join(expanded)))) + ".ip6.arpa."
 
