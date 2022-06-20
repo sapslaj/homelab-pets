f2ab73d2695920ec1d582207b7334cbe3339f73c
sapslaj <saps.laj@gmail.com>
2022-06-20 18:31:33 -0400

    fix ipv6_to_ptr bug and also add some tests

--- common/static-dns/modules/record_set/ipv6_to_ptr.py
+++ common/static-dns/modules/record_set/ipv6_to_ptr.py
@@ -3,20 +3,21 @@ import json
 import sys
 
 
-query = json.load(sys.stdin)
+def convert(ip):
+    partitions = ip.split("::")
+    expanded = partitions[0].split(":")
+    if len(partitions) == 2:
+        host_groups = partitions[1].split(":")
+        for _ in range(8 - (len(expanded) + len(host_groups))):
+            expanded.append("0000")
+        expanded.extend(host_groups)
+    expanded = [group.rjust(4, "0") for group in expanded]
+    return ".".join(reversed(list("".join(expanded)))) + ".ip6.arpa."
 
-ip = query["ipv6"]
-partitions = ip.split("::")
-expanded = partitions[0].split(":")
-if len(partitions) == 2:
-    host_groups = partitions[1].split(":")
-    for _ in range(8 - len(expanded) + len(host_groups)):
-        expanded.append("0000")
-    expanded.extend(host_groups)
-expanded = [group.rjust(4, "0") for group in expanded]
-ptr = ".".join(reversed(list("".join(expanded)))) + ".ip6.arpa."
-
-result = {
-    "ptr": ptr
-}
-json.dump(result, sys.stdout)
+if __name__ == "__main__":
+    query = json.load(sys.stdin)
+    ip = query["ipv6"]
+    result = {
+        "ptr": convert(ip),
+    }
+    json.dump(result, sys.stdout)
