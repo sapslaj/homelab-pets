316fcd09c8151da8d1e67107d8479ea681a653f4
sapslaj <saps.laj@gmail.com>
2023-03-05 02:09:47 -0500

    Add forward lookup filter to ZonePop config

--- zerotwo/kubernetes/zonepop_config/config.lua
+++ zerotwo/kubernetes/zonepop_config/config.lua
@@ -17,6 +17,21 @@ return {
         forward_zone_id = "Z00048261CEI1B6JY63KT",
         ipv4_reverse_zone_id = "Z00206652RDLR1KV5OQ39",
         ipv6_reverse_zone_id = "Z00734311E53TPLAI5AXC",
+        forward_lookup_filter = function(endpoint)
+          if endpoint.source_properties.dhcp_pool == "LAN_Management" then
+            return true
+          elseif endpoint.source_properties.dhcp_pool == "LAN_Servers" then
+            return not endpoint.hostname == "aqua"
+          elseif endpoint.source_properties.dhcp_pool == "LAN_Internal" then
+            local allowed_hostnames = {"homeassistant", "darkness", "playboy", "k3sdev", "steamdeck", "megumin"}
+            for _, v in pairs(allowed_hostnames) do
+              if v == endpoint.hostname then
+                return true
+              end
+            end
+          end
+          return false
+        end,
       },
     },
   },
