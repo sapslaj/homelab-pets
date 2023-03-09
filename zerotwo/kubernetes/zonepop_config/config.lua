65fa7e56fe59e8e54aa57de6c15ff43c1b45fa15
sapslaj <saps.laj@gmail.com>
2023-03-09 14:41:57 -0500

    Take advantage of ZP 0.4 logging

--- zerotwo/kubernetes/zonepop_config/config.lua
+++ zerotwo/kubernetes/zonepop_config/config.lua
@@ -1,3 +1,4 @@
+local log = require("log")
 return {
   sources = {
     vyos = {
@@ -6,6 +7,8 @@ return {
         host = os.getenv("VYOS_HOST"),
         username = os.getenv("VYOS_USERNAME"),
         password = os.getenv("VYOS_PASSWORD"),
+        record_ttl = 300,
+        collect_ipv6_neighbors = true,
       },
     },
   },
@@ -18,18 +21,34 @@ return {
         ipv4_reverse_zone_id = "Z00206652RDLR1KV5OQ39",
         ipv6_reverse_zone_id = "Z00734311E53TPLAI5AXC",
         forward_lookup_filter = function(endpoint)
+          local log_labels = {
+            filter_direction = "forward",
+            hostname = endpoint.hostname,
+            dhcp_pool = endpoint.source_properties.dhcp_pool,
+          }
           if endpoint.source_properties.dhcp_pool == "LAN_Management" then
+            log.info("endpoint is in LAN_Management, allowing", log_labels)
             return true
           elseif endpoint.source_properties.dhcp_pool == "LAN_Servers" then
-            return not endpoint.hostname == "aqua"
+            local skip_hostnames = {"aqua"}
+            for _, v in pairs(skip_hostnames) do
+              if v == endpoint.hostname then
+                log.info("endpoint is in LAN_Servers and should be skipped", log_labels)
+                return false
+              end
+            end
+            log.info("endpoint is in LAN_Servers", log_labels)
+            return true
           elseif endpoint.source_properties.dhcp_pool == "LAN_Internal" then
             local allowed_hostnames = {"homeassistant", "darkness", "playboy", "k3sdev", "steamdeck", "megumin"}
             for _, v in pairs(allowed_hostnames) do
               if v == endpoint.hostname then
+                log.info("endpoint is in LAN_Internal and is an allowed hostname", log_labels)
                 return true
               end
             end
           end
+          log.info("endpoint is not explicitly handled so skipping", log_labels)
           return false
         end,
       },
