06b9f9ba0869db3bdee83db11e8841b26beb9bdf
sapslaj <saps.laj@gmail.com>
2024-01-15 02:30:00 -0500

    do fancy stuff with zonepop

--- zerotwo/kubernetes/zonepop_config/config.lua
+++ zerotwo/kubernetes/zonepop_config/config.lua
@@ -1,4 +1,48 @@
 local log = require("log")
+
+local forward_lookup_filter = function(endpoint)
+  local log_labels = {
+    filter_direction = "forward",
+    hostname = endpoint.hostname,
+    dhcp_pool = endpoint.source_properties.dhcp_pool,
+  }
+  if endpoint.source_properties.static then
+    log.info("endpoint is static, allowing", log_labels)
+    return true
+  end
+  if endpoint.source_properties.dhcp_pool == "LAN_Management" then
+    log.info("endpoint is in LAN_Management, allowing", log_labels)
+    return true
+  elseif endpoint.source_properties.dhcp_pool == "LAN_Servers" then
+    local skip_hostnames = {"aqua"}
+    for _, v in pairs(skip_hostnames) do
+      if v == endpoint.hostname then
+        log.info("endpoint is in LAN_Servers and should be skipped", log_labels)
+        return false
+      end
+    end
+    log.info("endpoint is in LAN_Servers", log_labels)
+    return true
+  elseif endpoint.source_properties.dhcp_pool == "LAN_Internal" then
+    local allowed_hostnames = {"homeassistant", "darkness", "playboy", "k3sdev", "steamdeck", "megumin", "silverwolf"}
+    for _, v in pairs(allowed_hostnames) do
+      if v == endpoint.hostname then
+        log.info("endpoint is in LAN_Internal and is an allowed hostname", log_labels)
+        return true
+      end
+    end
+    local allowed_hostname_parts = {"BroadLink", "shellyht"}
+    for _, v in pairs(allowed_hostname_parts) do
+      if string.find(endpoint.hostname, v) then
+        log.info("endpoint is in LAN_Internal and contains allowed hostname part", log_labels)
+        return true
+      end
+    end
+  end
+  log.info("endpoint is not explicitly handled so skipping", log_labels)
+  return false
+end
+
 return {
   sources = {
     static = {
@@ -108,6 +152,32 @@ return {
     },
   },
   providers = {
+    rem_coredns = {
+      "hosts_file",
+      config = {
+        forward_lookup_filter = forward_lookup_filter,
+        record_suffix = ".sapslaj.xyz",
+        file = "/etc/coredns/zonepop.hosts",
+        ssh = {
+          host = "rem.sapslaj.xyz",
+          username = os.getenv("VYOS_USERNAME"),
+          password = os.getenv("VYOS_PASSWORD"),
+        }
+      }
+    },
+    ram_coredns = {
+      "hosts_file",
+      config = {
+        forward_lookup_filter = forward_lookup_filter,
+        record_suffix = ".sapslaj.xyz",
+        file = "/etc/coredns/zonepop.hosts",
+        ssh = {
+          host = "ram.sapslaj.xyz",
+          username = os.getenv("VYOS_USERNAME"),
+          password = os.getenv("VYOS_PASSWORD"),
+        }
+      }
+    },
     route53 = {
       "aws_route53",
       config = {
@@ -117,48 +187,7 @@ return {
         ipv6_reverse_zone_id = "Z00734311E53TPLAI5AXC",
         clean_ipv4_reverse_zone = true,
         clean_ipv6_reverse_zone = true,
-        forward_lookup_filter = function(endpoint)
-          local log_labels = {
-            filter_direction = "forward",
-            hostname = endpoint.hostname,
-            dhcp_pool = endpoint.source_properties.dhcp_pool,
-          }
-          if endpoint.source_properties.static then
-            log.info("endpoint is static, allowing", log_labels)
-            return true
-          end
-          if endpoint.source_properties.dhcp_pool == "LAN_Management" then
-            log.info("endpoint is in LAN_Management, allowing", log_labels)
-            return true
-          elseif endpoint.source_properties.dhcp_pool == "LAN_Servers" then
-            local skip_hostnames = {"aqua"}
-            for _, v in pairs(skip_hostnames) do
-              if v == endpoint.hostname then
-                log.info("endpoint is in LAN_Servers and should be skipped", log_labels)
-                return false
-              end
-            end
-            log.info("endpoint is in LAN_Servers", log_labels)
-            return true
-          elseif endpoint.source_properties.dhcp_pool == "LAN_Internal" then
-            local allowed_hostnames = {"homeassistant", "darkness", "playboy", "k3sdev", "steamdeck", "megumin", "silverwolf"}
-            for _, v in pairs(allowed_hostnames) do
-              if v == endpoint.hostname then
-                log.info("endpoint is in LAN_Internal and is an allowed hostname", log_labels)
-                return true
-              end
-            end
-            local allowed_hostname_parts = {"BroadLink", "shellyht"}
-            for _, v in pairs(allowed_hostname_parts) do
-              if string.find(endpoint.hostname, v) then
-                log.info("endpoint is in LAN_Internal and contains allowed hostname part", log_labels)
-                return true
-              end
-            end
-          end
-          log.info("endpoint is not explicitly handled so skipping", log_labels)
-          return false
-        end,
+        forward_lookup_filter = forward_lookup_filter,
       },
     },
   },
