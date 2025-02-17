4961c059ccf24b20b109881caa8a80531b586565
sapslaj <saps.laj@gmail.com>
2025-02-17 18:01:54 -0500

    soju tweaks after Pulumi refactoring

--- soju/index.ts
+++ soju/index.ts
@@ -1,33 +1,22 @@
 import * as path from "path";
-import { VyosLeasesHostLookup } from "../common/pulumi/components/proxmox-vm/host-lookup";
-import { ProxmoxVM } from "../common/pulumi/components/proxmox-vm/ProxmoxVM";
+
 import { BaseConfigTrait } from "../common/pulumi/components/proxmox-vm/BaseConfigTrait";
+import { ProxmoxVM } from "../common/pulumi/components/proxmox-vm/ProxmoxVM";
 
-(async () => {
-  const hostLookup = new VyosLeasesHostLookup({
-    sshConfig: {
-      host: process.env.VYOS_HOST,
-      username: process.env.VYOS_USERNAME,
-      password: process.env.VYOS_PASSWORD,
-    },
-  });
-
-  new ProxmoxVM("soju", {
-    name: "soju",
-    traits: [
-      new BaseConfigTrait("base", {
-        ansible: {
-          hostLookup,
-          rolePaths: [
-            path.join(__dirname, "./ansible/roles"),
-          ],
-          roles: [
-            {
-              role: "sapslaj.soju",
-            },
-          ],
-        },
-      }),
-    ],
-  });
-})();
+new ProxmoxVM("soju", {
+  name: "soju",
+  traits: [
+    new BaseConfigTrait("base", {
+      ansible: {
+        rolePaths: [
+          path.join(__dirname, "./ansible/roles"),
+        ],
+        roles: [
+          {
+            role: "sapslaj.soju",
+          },
+        ],
+      },
+    }),
+  ],
+});
