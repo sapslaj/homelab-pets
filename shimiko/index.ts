02c0d1ee0200685b7dca50b333cc7e3e347ad0da
sapslaj <saps.laj@gmail.com>
2025-08-12 18:36:16 -0400

    rip out Ansible from Shimiko
    only a whole lot of bumps along the way but it's fine

--- shimiko/index.ts
+++ shimiko/index.ts
@@ -1,13 +1,12 @@
-import * as path from "path";
-
 import * as aws from "@pulumi/aws";
 import { local } from "@pulumi/command";
-import { remote as remote_inputs } from "@pulumi/command/types/input";
 import * as pulumi from "@pulumi/pulumi";
 import * as time from "@pulumiverse/time";
-import { AnsibleProvisioner } from "@sapslaj/pulumi-ansible-provisioner";
+import * as mid from "@sapslaj/pulumi-mid";
 
 import { directoryHash } from "../common/pulumi/asset-utils";
+import { RsyncBackup } from "../common/pulumi/components/mid/RsyncBackup";
+import { SystemdUnit } from "../common/pulumi/components/mid/SystemdUnit";
 import { BaseConfigTrait } from "../common/pulumi/components/proxmox-vm/BaseConfigTrait";
 import { DNSRecordTrait } from "../common/pulumi/components/proxmox-vm/DNSRecordTrait";
 import { ProxmoxVM } from "../common/pulumi/components/proxmox-vm/ProxmoxVM";
@@ -19,23 +18,32 @@ const production = config.getBoolean("production");
 const vm = new ProxmoxVM("shimiko", {
   name: production ? "shimiko" : undefined,
   memory: {
-    dedicated: 4096,
+    dedicated: 1024,
   },
+  ignoreChanges: production
+    ? [
+      "initialization",
+    ]
+    : [],
   traits: [
     new BaseConfigTrait("base", {
       mid: {
-        midTarget: {
+        autoupdate: {
           enabled: true,
         },
         baselineUsers: {
-          // TODO: migrate from Ansible
-          enabled: false,
+          enabled: true,
+        },
+        midTarget: {
+          enabled: true,
+        },
+        nasClient: {
+          enabled: production,
+        },
+        openTelemetryCollector: {
+          enabled: true,
         },
         prometheusNodeExporter: {
-          // TODO: migrate from Ansible
-          enabled: false,
-        },
-        autoupdate: {
           enabled: true,
         },
         selfheal: {
@@ -44,6 +52,12 @@ const vm = new ProxmoxVM("shimiko", {
         vector: {
           enabled: true,
           sources: {
+            metrics_shimiko: {
+              type: "prometheus_scrape",
+              endpoints: ["http://localhost:9245/metrics"],
+              scrape_interval_secs: 60,
+              scrape_timeout_secs: 45,
+            },
             metrics_zonepop: {
               type: "prometheus_scrape",
               endpoints: ["http://localhost:9412/metrics"],
@@ -54,23 +68,7 @@ const vm = new ProxmoxVM("shimiko", {
         },
       },
       dnsRecord: !production,
-      ansible: {
-        clean: false,
-        base: {
-          nasClient: production,
-          rsyncBackup: production,
-          rsyncBackupConfig: production
-            ? {
-              jobs: [
-                {
-                  src: "/var/shimiko",
-                  dest: "/mnt/exos/volumes/shimiko/shimiko-data/",
-                },
-              ],
-            }
-            : undefined,
-        },
-      },
+      ansible: false,
       cloudImage: {
         diskConfig: {
           size: 32,
@@ -85,9 +83,12 @@ const dnsRecord = DNSRecordTrait.dnsRecordFor(vm);
 export const ipv4 = vm.ipv4;
 
 const shimikoBinaryBuild = new local.Command("shimiko-binary-build", {
-  create: `go build -o '${__dirname}/ansible/roles/shimiko/files/shimiko' '${__dirname}/cmd'`,
+  create: `go build -o '${__dirname}/shimiko' '${__dirname}/cmd'`,
   triggers: [
     directoryHash(__dirname),
+  ],
+  assetPaths: [
+    "shimiko",
   ],
 });
 
@@ -128,58 +129,219 @@ if (production) {
   acmeURL = "https://acme-v02.api.letsencrypt.org/directory";
 }
 
-new AnsibleProvisioner("shimiko-setup", {
-  connection: vm.connection as remote_inputs.ConnectionArgs,
-  clean: false,
-  rolePaths: [
-    path.join(__dirname, "ansible/roles"),
-  ],
-  preTasks: production
-    ? [
-      {
-        shell: {
-          cmd: `if [ ! -d /var/shimiko ]; then cp -rv /mnt/exos/volumes/shimiko/shimiko-data/shimiko /var/ ; fi`,
-        },
+const etcSysconfig = new mid.resource.File("/etc/sysconfig", {
+  connection: vm.connection,
+  path: "/etc/sysconfig",
+  ensure: "directory",
+}, {
+  deletedWith: vm,
+  retainOnDelete: true,
+});
+
+let preTasks: pulumi.Resource[] = [];
+
+if (production) {
+  preTasks.push(
+    new mid.resource.Exec("shimiko-restore-backup", {
+      connection: vm.connection,
+      create: {
+        command: [
+          "/bin/sh",
+          "-c",
+          `if [ ! -d /var/shimiko ]; then cp -rv /mnt/exos/volumes/shimiko/shimiko-data/shimiko /var/ ; fi`,
+        ],
       },
-    ]
-    : [],
-  roles: [
-    {
-      role: "shimiko",
-      vars: {
-        shimiko_env: {
-          AWS_REGION: "us-east-1",
-          AWS_ACCESS_KEY_ID: iamKeyShimiko.id,
-          AWS_SECRET_ACCESS_KEY: iamKeyShimiko.secret,
-          VYOS_USERNAME: process.env.VYOS_USERNAME, // FIXME: don't do this.
-          VYOS_PASSWORD: process.env.VYOS_PASSWORD, // FIXME: don't do this.
-          SHIMIKO_ACME_EMAIL: "alerts@sapslaj.com",
-          SHIMIKO_ACME_URL: acmeURL,
-          SHIMIKO_CERT_DOMAINS: production ? "shimiko.sapslaj.xyz" : dnsRecord.fullname,
-          SHIMIKO_RECONCILE_INTERVAL: production ? "1h" : "0s",
-        },
-      },
-    },
-    {
-      role: "zonepop",
-      vars: {
-        zonepop_env: {
-          AWS_REGION: "us-east-1",
-          AWS_ACCESS_KEY_ID: iamKeyZonepop.id,
-          AWS_SECRET_ACCESS_KEY: iamKeyZonepop.secret,
-          VYOS_HOST: "yor.sapslaj.xyz",
-          VYOS_USERNAME: process.env.VYOS_USERNAME, // FIXME: don't do this.
-          VYOS_PASSWORD: process.env.VYOS_PASSWORD, // FIXME: don't do this.
-        },
-      },
-    },
-  ],
-  triggers: [
-    shimikoBinaryBuild.id,
-  ],
+    }),
+  );
+}
+
+const varShimiko = new mid.resource.File("/var/shimiko", {
+  connection: vm.connection,
+  path: "/var/shimiko",
+  ensure: "directory",
 }, {
   dependsOn: [
-    shimikoBinaryBuild,
-    vm,
+    ...preTasks,
+  ],
+  deletedWith: vm,
+});
+
+if (production) {
+  new RsyncBackup("rsync-backup", {
+    connection: vm.connection,
+    backupJobs: [
+      {
+        src: "/var/shimiko",
+        dest: "/mnt/exos/volumes/shimiko/shimiko-data/",
+      },
+    ],
+  }, {
+    deletedWith: vm,
+    dependsOn: [
+      varShimiko,
+    ],
+  });
+}
+
+const shimikoBinary = new mid.resource.File("/usr/local/bin/shimiko", {
+  connection: vm.connection,
+  path: "/usr/local/bin/shimiko",
+  source: shimikoBinaryBuild.assets!.apply((assets) => assets!.shimiko),
+  mode: "a+x",
+}, {
+  deletedWith: vm,
+});
+
+const shimikoEnv = new mid.resource.File("/etc/sysconfig/shimiko.env", {
+  connection: vm.connection,
+  path: "/etc/sysconfig/shimiko.env",
+  content: pulumi.concat(
+    ...Object.entries({
+      AWS_ACCESS_KEY_ID: iamKeyShimiko.id,
+      AWS_REGION: "us-east-1",
+      AWS_SECRET_ACCESS_KEY: iamKeyShimiko.secret,
+      OTEL_EXPORTER_OTLP_ENDPOINT: "http://localhost:4317/v1/traces",
+      SHIMIKO_ACME_EMAIL: "alerts@sapslaj.com",
+      SHIMIKO_ACME_URL: acmeURL,
+      SHIMIKO_CERT_DOMAINS: production ? "shimiko.sapslaj.xyz" : dnsRecord.fullname,
+      SHIMIKO_HTTPS_PORT: "443",
+      SHIMIKO_HTTP_PORT: "80",
+      SHIMIKO_RECONCILE_INTERVAL: production ? "1h" : "0s",
+      VYOS_PASSWORD: process.env.VYOS_PASSWORD, // FIXME: don't do this.
+      VYOS_USERNAME: process.env.VYOS_USERNAME, // FIXME: don't do this.
+    }).map(([key, value]) => {
+      return pulumi.concat(key, "='", value, "'\n");
+    }),
+  ),
+}, {
+  deletedWith: vm,
+  dependsOn: [
+    etcSysconfig,
+  ],
+});
+
+const shimikoService = new SystemdUnit("shimiko-server.service", {
+  connection: vm.connection,
+  name: "shimiko-server.service",
+  daemonReload: true,
+  enabled: true,
+  ensure: "started",
+  unit: {
+    "Description": "Shimiko server",
+    "After": "network-online.target",
+  },
+  service: {
+    "Type": "simple",
+    "EnvironmentFile": shimikoEnv.path,
+    "WorkingDirectory": varShimiko.path,
+    "ExecStart": pulumi.interpolate`${shimikoBinary.path} server`,
+    "Restart": "always",
+    "RestartSec": "1",
+  },
+  install: {
+    "WantedBy": "multi-user.target",
+  },
+  triggers: {
+    refresh: [
+      varShimiko.triggers.lastChanged,
+      shimikoBinary.triggers.lastChanged,
+      shimikoEnv.triggers.lastChanged,
+    ],
+  },
+  config: {
+    check: false,
+  },
+}, {
+  deletedWith: vm,
+  dependsOn: [
+    varShimiko,
+    shimikoBinary,
+    shimikoEnv,
+  ],
+});
+
+const etcZonepop = new mid.resource.File("/etc/zonepop", {
+  connection: vm.connection,
+  path: "/etc/zonepop",
+  ensure: "directory",
+}, {
+  deletedWith: vm,
+});
+
+const zonepopConfig = new mid.resource.File("/etc/zonepop/config.lua", {
+  connection: vm.connection,
+  path: "/etc/zonepop/config.lua",
+  source: new pulumi.asset.FileAsset("./zonepop-config.lua"),
+}, {
+  deletedWith: vm,
+  dependsOn: [
+    etcZonepop,
+  ],
+});
+
+const zonepopBinary = new mid.resource.File("/usr/local/bin/zonepop", {
+  connection: vm.connection,
+  path: "/usr/local/bin/zonepop",
+  remoteSource: "https://misc.sapslaj.xyz/zonepop",
+  mode: "a+x",
+}, {
+  deletedWith: vm,
+});
+
+const zonepopEnv = new mid.resource.File("/etc/sysconfig/zonepop.env", {
+  connection: vm.connection,
+  path: "/etc/sysconfig/zonepop.env",
+  content: pulumi.concat(
+    ...Object.entries({
+      AWS_REGION: "us-east-1",
+      AWS_ACCESS_KEY_ID: iamKeyZonepop.id,
+      AWS_SECRET_ACCESS_KEY: iamKeyZonepop.secret,
+      VYOS_HOST: "yor.sapslaj.xyz",
+      VYOS_USERNAME: process.env.VYOS_USERNAME, // FIXME: don't do this.
+      VYOS_PASSWORD: process.env.VYOS_PASSWORD, // FIXME: don't do this.
+    }).map(([key, value]) => {
+      return pulumi.concat(key, "='", value, "'\n");
+    }),
+  ),
+}, {
+  deletedWith: vm,
+  dependsOn: [
+    etcSysconfig,
+  ],
+});
+
+const zonepopService = new SystemdUnit("zonepop.service", {
+  connection: vm.connection,
+  name: "zonepop.service",
+  daemonReload: true,
+  enabled: true,
+  ensure: "started",
+  unit: {
+    "Description": "Zonepop",
+    "After": "network-online.target",
+  },
+  service: {
+    "Type": "simple",
+    "EnvironmentFile": zonepopEnv.path,
+    "ExecStart": pulumi.interpolate`${zonepopBinary.path} -config-file ${zonepopConfig.path}`,
+    "Restart": "always",
+    "RestartSec": "1",
+  },
+  install: {
+    "WantedBy": "multi-user.target",
+  },
+  triggers: {
+    refresh: [
+      zonepopConfig.triggers.lastChanged,
+      zonepopBinary.triggers.lastChanged,
+      zonepopEnv.triggers.lastChanged,
+    ],
+  },
+}, {
+  deletedWith: vm,
+  dependsOn: [
+    zonepopConfig,
+    zonepopBinary,
+    zonepopEnv,
   ],
 });
