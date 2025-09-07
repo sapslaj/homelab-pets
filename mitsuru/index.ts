b9ea64fc9e4935c1c859a1757e0153e7d3b83579
sapslaj <saps.laj@gmail.com>
2025-09-07 10:11:36 -0400

    Rclone on mitsuru

--- mitsuru/index.ts
+++ mitsuru/index.ts
@@ -6,10 +6,12 @@ import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 import * as mid from "@sapslaj/pulumi-mid";
 
+import { getSecretValue } from "../common/pulumi/components/infisical";
 import { Autoupdate } from "../common/pulumi/components/mid/Autoupdate";
 import { BaselineUsers } from "../common/pulumi/components/mid/BaselineUsers";
 import { MidTarget } from "../common/pulumi/components/mid/MidTarget";
 import { PrometheusNodeExporter } from "../common/pulumi/components/mid/PrometheusNodeExporter";
+import { Rclone } from "../common/pulumi/components/mid/Rclone";
 import { SystemdUnit } from "../common/pulumi/components/mid/SystemdUnit";
 import { Vector } from "../common/pulumi/components/mid/Vector";
 
@@ -127,5 +129,75 @@ new mid.resource.SystemdService("nfs-kernel-server.service", {
   dependsOn: [
     nfsPackages,
     nfsExports,
+  ],
+});
+
+new Rclone("ganyu", {
+  connection,
+  configs: [
+    {
+      name: "wasabi-use1",
+      properties: {
+        type: "s3",
+        endpoint: "s3.wasabisys.com",
+        region: "us-east-1",
+        access_key_id: getSecretValue({
+          folder: "/wasabi/rclone-mitsuru",
+          key: "AWS_ACCESS_KEY_ID",
+        }),
+        secret_access_key: getSecretValue({
+          folder: "/wasabi/rclone-mitsuru",
+          key: "AWS_SECRET_ACCESS_KEY",
+        }),
+      },
+    },
+    {
+      name: "red-nfs",
+      properties: {
+        type: "crypt",
+        remote: "wasabi-use1:sapslaj-homelab-backups/red-nfs",
+        password: getSecretValue({
+          folder: "/rclone",
+          key: "password",
+        }),
+        password2: getSecretValue({
+          folder: "/rclone",
+          key: "password2",
+        }),
+      },
+    },
+    {
+      name: "red-shortrack",
+      properties: {
+        type: "crypt",
+        remote: "wasabi-use1:sapslaj-homelab-backups/red-shortrack",
+        password: getSecretValue({
+          folder: "/rclone",
+          key: "password",
+        }),
+        password2: getSecretValue({
+          folder: "/rclone",
+          key: "password2",
+        }),
+      },
+    },
+  ],
+  jobs: {
+    "red-nfs": {
+      src: "/red/nfs",
+      dest: "red-nfs:",
+      enabled: true,
+      ensure: "started",
+    },
+    "red-shortrack": {
+      src: "/red/shortrack",
+      dest: "red-shortrack:",
+      enabled: true,
+      ensure: "started",
+    },
+  },
+}, {
+  dependsOn: [
+    midTarget,
   ],
 });
